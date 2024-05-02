bedcf54437b7352d3ffa66ac39e59beccbfc2f67
JohnyBreak <rashkost@mail.ru>
2024-05-02 23:20:20 +0700

    передвижение персонажа по сетке (надо доделать анимации)

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     [SerializeField] private Animator _animator;
     [SerializeField] private Field _field;
     [SerializeField] private Barrel _BurrelPrefab;
+    [SerializeField] private LayerMask _layerMask;
 
     private int _verticalHash = Animator.StringToHash("Vertical"); 
     private int _horizontalHash = Animator.StringToHash("Horizontal");
@@ -18,6 +19,16 @@ public class Player : MonoBehaviour
     private Vector2 _moveVector;
 
     private bool _alive = true;
+
+    private Vector3 _movePosition;
+
+    private float _cashedX;
+    private float _cashedY;
+
+    private void Awake()
+    {
+        _movePosition = transform.position;
+    }
 
     public void Kill() 
     {
@@ -32,24 +43,69 @@ public class Player : MonoBehaviour
             return;
         }
 
+
+        transform.position = Vector3.MoveTowards(transform.position, _movePosition, _moveSpeed * Time.deltaTime);
+
         var x = Input.GetAxisRaw("Horizontal");
         var y = Input.GetAxisRaw("Vertical");
+        
 
-        _moveVector = new Vector2(x, y).normalized * _moveSpeed;
-
-        _animator.SetFloat(_horizontalHash, x);
-        _animator.SetFloat(_verticalHash, y);
-
-        var magnitude = _moveVector.magnitude;
-        _animator.SetFloat(_speedHash, magnitude);
-
-        if (magnitude > 0.1f) 
+        if (Vector3.Distance(transform.position, _movePosition) <= 0.05f)
         {
-            _animator.SetFloat(_lastHorizontalHash, x);
-            _animator.SetFloat(_lastVerticalHash, y);
+            _animator.SetFloat(_lastHorizontalHash, _cashedX);
+            _animator.SetFloat(_lastVerticalHash, _cashedY);
+
+            _animator.SetFloat(_speedHash, 0);
+
+            //_cashedX = 0;
+            //_cashedY = 0;
+
+            if (x != 0)
+            {
+                if (!Physics2D.OverlapCircle(_movePosition + new Vector3(x, 0, 0), 0.2f, _layerMask))
+                {
+                    _movePosition += new Vector3(x, 0, 0);
+                }
+            }
+            else if (y != 0)
+            {
+                if (!Physics2D.OverlapCircle(_movePosition + new Vector3(0, y, 0), 0.2f, _layerMask))
+                {
+                    _movePosition += new Vector3(0, y, 0);
+                }
+            }
+        }
+        else 
+        {
+            _cashedX = x;
+            _cashedY = y;
+
+            _animator.SetFloat(_horizontalHash, _cashedX);
+            _animator.SetFloat(_verticalHash, _cashedY);
+
+            _animator.SetFloat(_speedHash, 1);
         }
 
-        _characterController.Move(_moveVector);
+
+
+        //var x = Input.GetAxisRaw("Horizontal");
+        //var y = Input.GetAxisRaw("Vertical");
+
+        //_moveVector = new Vector2(x, y).normalized * _moveSpeed;
+
+        //_animator.SetFloat(_horizontalHash, x);
+        //_animator.SetFloat(_verticalHash, y);
+
+        //var magnitude = _moveVector.magnitude;
+        //_animator.SetFloat(_speedHash, magnitude);
+
+        //if (magnitude > 0.1f) 
+        //{
+        //    _animator.SetFloat(_lastHorizontalHash, x);
+        //    _animator.SetFloat(_lastVerticalHash, y);
+        //}
+
+        //_characterController.Move(_moveVector);
 
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
