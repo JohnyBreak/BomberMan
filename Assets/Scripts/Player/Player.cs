e56c42fba0ba8a53d1c7c80ae435355d7b1e3104
JohnyBreak <rashkost@mail.ru>
2024-05-22 22:00:00 +0700

    спавн плеера из конфига уровня

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,70 +4,71 @@ using Zenject;
 public class Player : MonoBehaviour
 {
     [SerializeField] private CharacterController2D _characterController;
-    
-    [SerializeField] private Animator _animator;
-    [SerializeField] private LayerMask _layerMask;
 
-    private int _verticalHash = Animator.StringToHash("Vertical"); 
+    [SerializeField] private Animator _animator;
+
+    private int _verticalHash = Animator.StringToHash("Vertical");
     private int _horizontalHash = Animator.StringToHash("Horizontal");
     private int _lastVerticalHash = Animator.StringToHash("LastVertical");
     private int _lastHorizontalHash = Animator.StringToHash("LastHorizontal");
     private int _speedHash = Animator.StringToHash("Speed");
     private int _deathHash = Animator.StringToHash("Death");
 
+    private Vector2 _inputVector;
     private Vector2 _moveVector;
-    //private float _moveSpeed = 2f;
     private PlayerStats _stats;
     private GameStateController _gameStateController;
 
     [Inject]
-    private void Construct(PlayerStats stats, GameStateController stateController) 
+    private void Construct(PlayerStats stats, GameStateController stateController)
     {
         _stats = stats;
         _gameStateController = stateController;
     }
 
-    private Vector3 _movePosition;
-
-    private float _cashedX;
-    private float _cashedY;
-
-    private void Awake()
-    {
-        _movePosition = transform.position;
-    }
-
-    public void Kill() 
+    public void Kill()
     {
         _gameStateController.SetState(LoseState.Name);
-        //_alive = false;
         _animator.SetTrigger(_deathHash);
     }
 
     private void Update()
     {
-        if (_gameStateController.IsCurrentState(GamePlayState.Name) == false) 
+        if (_gameStateController.IsCurrentState(GamePlayState.Name) == false)
         {
             return;
         }
-        
-        var x = Input.GetAxisRaw("Horizontal");
-        var y = Input.GetAxisRaw("Vertical");
 
-        _moveVector = new Vector2(x, y).normalized * _stats.PlayerMoveSpeed;
+        GetInput();
 
-        _animator.SetFloat(_horizontalHash, x);
-        _animator.SetFloat(_verticalHash, y);
+        HandleAnimations();
+
+        Move();
+    }
+
+    private void GetInput()
+    {
+        _inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _moveVector = _inputVector.normalized * _stats.PlayerMoveSpeed;
+    }
+
+    private void HandleAnimations() 
+    {
+        _animator.SetFloat(_horizontalHash, _inputVector.x);
+        _animator.SetFloat(_verticalHash, _inputVector.y);
 
         var magnitude = _moveVector.magnitude;
         _animator.SetFloat(_speedHash, magnitude);
 
-        if (magnitude > 0.1f) 
+        if (magnitude > 0.1f)
         {
-            _animator.SetFloat(_lastHorizontalHash, x);
-            _animator.SetFloat(_lastVerticalHash, y);
+            _animator.SetFloat(_lastHorizontalHash, _inputVector.x);
+            _animator.SetFloat(_lastVerticalHash, _inputVector.y);
         }
+    }
 
+    private void Move()
+    {
         _characterController.Move(_moveVector);
     }
 }
